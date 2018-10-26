85848d0edee47bc8d6d3ef842ef85433b01c0dc2
Francisco López <fclopezjurado@gmail.com>
2018-10-26 15:04:57 +0200

    Adding more examples in casting file.

--- Chapter 1/casting.php
+++ Chapter 1/casting.php
@@ -5,6 +5,16 @@
  * User: fljurado
  * Date: 25/10/18
  * Time: 9:38
+ *
+ * Casting test. Available castings:
+ * (int), (integer) - cast to integer
+ * (bool), (boolean) - cast to boolean
+ * (float), (double), (real) - cast to float
+ * (string) - cast to string
+ * (array) - cast to array
+ * (object) - cast to object
+ * (unset) - cast to NULL
+ * https://secure.php.net/manual/en/language.types.type-juggling.php
  */
 
 $handler = curl_init();
@@ -24,7 +34,6 @@ var_dump((bool)array());
 var_dump((bool)"false");
 var_dump(boolval(789));
 var_dump((bool)$handler);
-
 var_dump('--------------------');
 
 /**
@@ -46,8 +55,12 @@ var_dump((int)"false");
 var_dump(intval($handler));
 var_dump(intval(NAN));
 var_dump(intval(INF));
+/**
+ * The reason behind this is that some numbers are rational in base 10 but are irrational in base 2. Although 0.7 can
+ * be expressed as a rational number in base 10, when expressed in base 2 it is irrational. Because there are a
+ * limited number of bits available to store the number, it is inevitable that some loss of precision will occur.
+ */
 var_dump((int)((0.1 + 0.7) * 10)); # Returns 7, because floating point precision.
-
 var_dump('--------------------');
 
 /**
@@ -71,7 +84,6 @@ var_dump(floatval($handler));
 var_dump(floatval(NAN));
 var_dump(floatval(INF));
 var_dump((float)((0.1 + 0.7) * 10)); # Returns 8, because floating point precision.
-
 var_dump('--------------------');
 
 /**
@@ -101,13 +113,13 @@ var_dump(strval($handler));
 var_dump(strval(NAN));
 var_dump(strval(INF));
 var_dump((string)((0.1 + 0.7) * 10)); # Returns 8, because floating point precision.
-
 var_dump('--------------------');
 
 /**
  * Casting to NULL, when a variable is unset.
  */
 var_dump((unset)$test);
+var_dump('--------------------');
 
 /**
  * Casting to real. This casting is similar to "double" casting.
@@ -129,3 +141,46 @@ var_dump((real)($handler));
 var_dump((real)NAN);
 var_dump((real)INF);
 var_dump((real)((0.1 + 0.7) * 10)); # Returns 8, because floating point precision.
+var_dump('--------------------');
+
+/**
+ * Automatic type conversion examples.
+ */
+$foo = "1";
+var_dump($foo);
+$foo *= 2;
+var_dump($foo);
+$foo = $foo * 1.3;
+var_dump($foo);
+$foo = 5 * "10 Little Piggies";
+var_dump($foo);
+$foo = 5 * "10 Small Pigs";
+var_dump($foo);
+
+# The behaviour of an automatic conversion to array is currently undefined.
+$a = 'car';
+$a[0] = 'b';
+var_dump($a);
+var_dump('--------------------');
+
+/**
+ * String conversion to numbers.
+ */
+$examples = [
+    (1 + "10.5"),
+    (1 + "-1.3e3"),
+    (1 + "bob-1.3e3"),
+    (1 + "bob3"),
+    (1 + "10 Small Pigs"),
+    (4 + "10.2 Little Piggies"),
+    ("10.0 pigs " + 1),
+    ("10.0 pigs " + 1.0),
+    "12 o clock",
+    "Half past 12",
+    "12.30",
+    "7.2e2 minutes after midnight"
+];
+foreach ($examples as $example) {
+    $result = 0 + $example;
+    var_dump($result);
+}
