59c0d1e349c55a9afda34573778f57de306fa913
Francisco López <fclopezjurado@gmail.com>
2018-10-29 17:47:51 +0100

    Improvement outputs of source code defined for this script.

--- Chapter 1/magic_constants.php
+++ Chapter 1/magic_constants.php
@@ -10,11 +10,35 @@
  * https://secure.php.net/manual/en/language.constants.predefined.php
  */
 
+namespace NamespaceTest;
+
+trait MyTrait
+{
+    public function traitFunction() {
+        var_dump(__TRAIT__);
+        var_dump(__METHOD__);
+        var_dump(__FUNCTION__);
+    }
+}
+
+class MyClass1
+{
+    use MyTrait;
+
+    public function classFunction()
+    {
+        var_dump(__CLASS__);
+        var_dump(__METHOD__);
+        var_dump(__FUNCTION__);
+    }
+}
+
+$myClass1Object = new MyClass1();
+
 var_dump(__LINE__);
 var_dump(__FILE__);
 var_dump(__DIR__);
-var_dump(__FUNCTION__);
-var_dump(__CLASS__);
-var_dump(__TRAIT__);
-var_dump(__METHOD__);
 var_dump(__NAMESPACE__);
+
+$myClass1Object->classFunction();
+$myClass1Object->traitFunction();
