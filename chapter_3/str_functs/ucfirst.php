5531f054b2ad7251ef133e735c9571c1593063a1
Francisco López <fclopezjurado@gmail.com>
2018-12-11 09:44:47 +0100

    Adding new scripts to chapter 3 to manage strings.

--- chapter_3/str_functs/ucfirst.php
+++ chapter_3/str_functs/ucfirst.php
@@ -1,7 +1,20 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: fljurado
  * Date: 8/11/18
  * Time: 16:32
+ *
+ * Make a string's first character uppercase.
+ * http://php.net/manual/en/function.ucfirst.php
+ * Returns a string with the first character of str capitalized, if that character is alphabetic.
+ * Note that 'alphabetic' is determined by the current locale. For instance, in the default "C" locale characters such
+ * as umlaut-a (ä) will not be converted.
+ * @param string $str the input string.
+ * @return string the resulting string.
  */
+
+var_dump(ucfirst('hello world!'));
+var_dump(ucfirst('HELLO WORLD!'));
+var_dump(ucfirst(strtolower('HELLO WORLD!')));
--- chapter_3/str_functs/vfprintf.php
+++ chapter_3/str_functs/vfprintf.php
@@ -1,7 +1,23 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: fljurado
  * Date: 8/11/18
  * Time: 16:32
+ *
+ * Write a formatted string to a stream.
+ * http://php.net/manual/en/function.vfprintf.php
+ * Write a string produced according to format to the stream resource specified by handle. Operates as fprintf() but
+ * accepts an array of arguments, rather than a variable number of arguments.
+ * @param resource $handle
+ * @param string $format see sprintf() for a description of format.
+ * @param array $args
+ * @return int the length of the outputted string.
  */
+
+if (!($fp = fopen('../../output.txt', 'w')))
+    return;
+
+// will write the formatted ISO date to date.txt
+var_dump(vfprintf($fp, "%04d-%02d-%02d", array(2018, 12, 11)));
