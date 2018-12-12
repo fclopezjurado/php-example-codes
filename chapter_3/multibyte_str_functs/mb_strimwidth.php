8d3b5d201cbe94d3c04f1da39f689a2eebf258e8
Francisco López <fclopezjurado@gmail.com>
2018-12-12 18:02:43 +0100

    Adding new scripts to chapter 3 to manage strings.

--- chapter_3/multibyte_str_functs/mb_strimwidth.php
+++ chapter_3/multibyte_str_functs/mb_strimwidth.php
@@ -1,7 +1,21 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: fljurado
  * Date: 8/11/18
  * Time: 16:50
+ *
+ * Get truncated string with specified width.
+ * http://php.net/manual/en/function.mb-strimwidth.php
+ * @param string $str the string being decoded.
+ * @param int $start the start position offset. Number of characters from the beginning of string
+ * (first character is 0), or if start is negative, number of characters from the end of the string.
+ * @param int $width the width of the desired trim. Negative widths count from the end of the string.
+ * @param string $trimmarker a string that is added to the end of string when string is truncated.
+ * @param string $encoding the encoding parameter is the character encoding. If it is omitted, the internal character
+ * encoding value will be used.
+ * @return string the truncated string. If trimmarker is set, trimmarker replaces the last chars to match the width.
  */
+
+var_dump(mb_strimwidth('Hello World', 0, 10, '...'));
--- chapter_3/multibyte_str_functs/mb_stristr.php
+++ chapter_3/multibyte_str_functs/mb_stristr.php
@@ -1,7 +1,25 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: fljurado
  * Date: 8/11/18
  * Time: 16:50
+ *
+ * Finds first occurrence of a string within another, case insensitive.
+ * http://php.net/manual/en/function.mb-stristr.php
+ * mb_stristr() finds the first occurrence of needle in haystack and returns the portion of haystack. Unlike
+ * mb_strstr(), mb_stristr() is case-insensitive. If needle is not found, it returns FALSE.
+ * @param string $haystack the string from which to get the first occurrence of needle.
+ * @param string $needle the string to find in haystack.
+ * @param bool $before_needle determines which portion of haystack this function returns. If set to TRUE, it returns
+ * all of haystack from the beginning to the first occurrence of needle (excluding needle). If set to FALSE, it returns
+ * all of haystack from the first occurrence of needle to the end (including needle).
+ * @param string $encoding character encoding name to use. If it is omitted, internal character encoding is used.
+ * @return string|bool the portion of haystack, or FALSE if needle is not found.
  */
+
+var_dump(mb_stristr('española','ol',false));
+var_dump(mb_stristr('española','ol',true,'UTF-8'));
+var_dump(mb_stristr('española','OL',false,'UTF-8'));
+var_dump(mb_stristr('española','an',false,'UTF-8'));
