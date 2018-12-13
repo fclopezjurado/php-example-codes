3ab1c930086673817e63dc0b49081b994041885e
Francisco López <fclopezjurado@gmail.com>
2018-12-13 17:00:20 +0100

    Adding new scripts to chapter 3 to manage strings.

--- chapter_3/multibyte_str_functs/mb_strtoupper.php
+++ chapter_3/multibyte_str_functs/mb_strtoupper.php
@@ -1,7 +1,17 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: fljurado
  * Date: 8/11/18
  * Time: 16:51
+ *
+ * Make a string uppercase.
+ * http://php.net/manual/en/function.mb-strtoupper.php
+ * @param string $str the string being uppercased.
+ * @param string $encoding the encoding parameter is the character encoding. If it is omitted, the internal character
+ * encoding value will be used.
  */
+
+var_dump(mb_strtoupper('Mary Had A Little Lamb and She LOVED It So'));
+var_dump(mb_strtoupper('Τάχιστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός', 'UTF-8'));
