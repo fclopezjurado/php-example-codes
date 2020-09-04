d2c1c9bccb184dc88d99eaeb7519791bf449e8ed
Francisco López <fclopezjurado@gmail.com>
2020-09-04 19:31:54 +0200

    Adding improvements and new scripts to chapter 3.

--- chapter_3/control_chars.php
+++ chapter_3/control_chars.php
@@ -20,3 +20,11 @@ var_dump("octal code: \040"); // \040 in octal is whitespace.
 var_dump("unicode char: \u{1F418}");
 var_dump("vertical tab: \v");
 var_dump("backslash char: \\");
+var_dump("space char: \040");
+var_dump("space char: \40");
+var_dump("back reference: \7");
+var_dump("might be a back reference, or another way of writing a tab: \11");
+var_dump("tab: \011");
+var_dump("tab followed by character 3: \0113");
+var_dump("character with octal code 113 (since there can be no more than 99 back references): \113");
+var_dump("byte consisting entirely of 1 bits: \377");
